4b68e134c16405e547df42c3d993a0d8d89214fc
JacquesN <jacques@cadeaudelamaison.com>
2024-02-04 23:05:23 +0100

    basic

---
+++ value.go
@@ -0,0 +1,9 @@
+package main 
+import "fmt"
+
+func main(){
+	fmt.Println("go" + "test")
+	fmt.Println(1+0.000000009)
+	fmt.Println(1)
+
+}
---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "math"
+
+var ad = 100
+func main(){
+	var b int
+	fmt.Println(b)
+	b=5
+	var a string
+	fmt.Println(a)
+	a = "test"
+	fmt.Println(a)
+
+	var c = 10
+	fmt.Println(c*b)
+	fmt.Println(ad+c)
+ad = 200
+	const foo = 200 
+	fmt.Println(foo)
+}
+
+fmt.Println(ad)
